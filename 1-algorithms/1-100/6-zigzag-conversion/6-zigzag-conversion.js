bc87c49cbd2db5c93aaf2d01418b76643ceef70d
LevDoesCode <levdoescode@gmail.com>
2023-10-18 23:13:07 -0500

    Worked on possible solution

--- 1-algorithms/1-100/6-zigzag-conversion/6-zigzag-conversion.js
+++ 1-algorithms/1-100/6-zigzag-conversion/6-zigzag-conversion.js
@@ -1,14 +1,25 @@
 var convert = function(s, numRows) {
     let reverted = "";
-    const len = s.length;
-    for (let pos = 0, i = 0, j = 0; pos < len; pos += 1) {
-        
-        console.log(pos, i, j);
-        i += numRows;
-        j = numRows - 2
+    //const len = s.length;
+    const bridge = numRows - 2;
+    let i = 0;
+    let row = 0;
+    let step = 0;
+    let pos = 0;
+    while (i < s.length) {
+        reverted += s[pos];
+        pos += (numRows - row) + (bridge - step);
+        if (pos > s.length) {
+            if (row > 0 && row < numRows - 1) {
+                step += 1;
+            }
+            row += 1;
+            pos = row;
+        }
+        i += 1;
     }
     return reverted;
 };
 
-console.log(convert("PAYPALISHIRING"), 3);
-console.log(convert("PAYPALISHIRING"), 4);
+console.log(convert("PAYPALISHIRING", 3));
+console.log(convert("PAYPALISHIRING", 4));
