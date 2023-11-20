a619060c8b582f394483c2f0f55103bd0af8732e
LevDoesCode <levdoescode@gmail.com>
2023-11-19 22:22:46 -0500

    Working recursion solution using a cache

--- 1-algorithms/1-100/10-regular-exp-match/10-regular-exp-match4.js
+++ 1-algorithms/1-100/10-regular-exp-match/10-regular-exp-match4.js
@@ -1,35 +1,40 @@
 // Using recursion and caching
 // https://www.youtube.com/watch?v=HAA8mgxlov8
+// In JavaScript we can't have an ordered pair as the key, so we need to turn it to a string
 var isMatch = function(s, p) {
-    const cache = {};
+    const memo = new Map(); // Use a Map to store cached results
+
     const dfs = function(i, j) {
-        if (cache[(i, j)] != undefined) {
-            return cache[(i, j)];
+        if (memo.has(`${i}-${j}`)) {
+            return memo.get(`${i}-${j}`);
         }
+
+        let result;
+
         if (i >= s.length && j >= p.length) {
-            return true;
-        }
-
-        if (j >= p.length) {
-            return false;
-        }
-
-        let match = i < s.length && (s[i] === p[j] || p[j] === '.');
-        if (j + 1 < p.length && p[j + 1] === '*') {
-            const zeroOrMore = dfs(i, j + 2) || (match && dfs(i + 1, j))
-            cache[(i, j)] = zeroOrMore;
-            if (zeroOrMore) {
-                return cache[(i, j)];
+            result = true;
+        } else if (j >= p.length) {
+            result = false;
+        } else {
+            let match = i < s.length && (s[i] === p[j] || p[j] === '.');
+            if (p[j + 1] === '*') {
+                if (dfs(i, j + 2) || (match && dfs(i + 1, j))) {
+                    result = true;
+                } else {
+                    result = false;
+                }
+            } else if (match) {
+                result = dfs(i + 1, j + 1);
+            } else {
+                result = false;
             }
         }
 
-        if (match) {
-            cache[(i, j)] = dfs(i + 1, j + 1);
-            return cache[(i, j)];
-        }
-        cache[(i, j)] = false;
-        return false;
-    }
+        // Cache the result before returning
+        memo.set(`${i}-${j}`, result);
+        return result;
+    };
+
     return dfs(0, 0);
 };
 
