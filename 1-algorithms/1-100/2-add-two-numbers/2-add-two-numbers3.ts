0f89e829032f7a7596eb5c9497305f4517fab68f
LevDoesCode <levdoescode@gmail.com>
2025-01-13 17:37:40 -0500

    Method with multiple functions

---
+++ 1-algorithms/1-100/2-add-two-numbers/2-add-two-numbers3.ts
@@ -0,0 +1,42 @@
+class ListNode {
+    val: number;
+    next: ListNode | null;
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.next = next === undefined ? null : next;
+    }
+}
+
+function getNumberFromLinkedList(node: ListNode | null): number {
+    let number = "";
+    while (node) {
+        number = node.val + number;
+        node = node.next;
+    }
+    return parseInt(number) || 0;
+}
+
+function createLinkedList(number: number): ListNode {
+    let head: ListNode | null = null;
+    let tail: ListNode | null = null;
+    while (number > 0) {
+        const node = new ListNode(number % 10);
+        if (!head) {
+            head = node;
+            tail = node;
+        } else {
+            tail!.next = node;
+            tail = node;
+        }
+        number = Math.floor(number / 10);
+    }
+    return head!;
+}
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    return createLinkedList(getNumberFromLinkedList(l1) + getNumberFromLinkedList(l2));
+}
+
+const l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+const l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+console.log(addTwoNumbers(l1, l2)); // 7 -> 0 -> 8
